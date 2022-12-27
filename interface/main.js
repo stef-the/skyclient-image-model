f7d2bdfb9c0c01547050bc545777250a65e6c79a
stef <stefthedoggo4@gmail.com>
2022-12-27 20:40:28 +0000

    finally

---
+++ interface/main.js
@@ -0,0 +1,70 @@
+// More API functions here:
+// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
+
+// the link to your model provided by Teachable Machine export panel
+const URL = "https://teachablemachine.withgoogle.com/models/h104zsiHQ/";
+
+let model, webcam, labelContainer, maxPredictions;
+
+// Load the image model and setup the webcam
+async function init() {
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
+
+  // load the model and metadata
+  // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+  // or files from your local hard drive
+  // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+  model = await tmImage.load(modelURL, metadataURL);
+  maxPredictions = model.getTotalClasses();
+
+  // Convenience function to setup a webcam
+  const flip = true; // whether to flip the webcam
+  webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+  await webcam.setup(); // request access to the webcam
+  await webcam.play();
+  window.requestAnimationFrame(loop);
+
+  model = await tmImage.load(modelURL, metadataURL);
+  maxPredictions = model.getTotalClasses();
+
+  // append elements to the DOM
+  labelContainer = document.getElementById("label-container");
+  for (let i = 0; i < maxPredictions; i++) {
+    // and class labels
+    labelContainer.appendChild(document.createElement("div"));
+  }
+}
+
+async function loop() {
+  webcam.update(); // update the webcam frame
+  await predict();
+  window.requestAnimationFrame(loop);
+}
+
+// run the webcam image through the image model
+async function predict() {
+  // predict can take in an image, video or canvas html element
+
+  const input = document.getElementById("imgselect");
+  input.click();
+
+  const image = document.getElementById("weewoo");
+  const reader = new FileReader();
+
+  reader.addEventListener(
+    "load",
+    () => {
+      image.src = reader.result;
+    },
+    false
+  );
+  reader.readAsDataURL(input.files[0]);
+
+  const predictions = await model.predict(image);
+  console.log(predictions);
+
+  predictions.forEach((prediction) => {
+    console.log(prediction);
+  });
+}
