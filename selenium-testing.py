d13f7c798e1e26d4af1b05111bf7f4edda621671
stef <stefthedoggo4@gmail.com>
2022-12-27 17:40:35 +0000

    switch to geckodriver, fix urls

--- selenium-testing.py
+++ selenium-testing.py
@@ -1,9 +1,8 @@
-import os
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://www.google.com")
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
 url = 'https://teachablemachine.withgoogle.com/train/image'
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+driver.get(url)
