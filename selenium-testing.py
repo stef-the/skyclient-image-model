a7d88c78c363addfb68a3411e38c3d87669d3e6f
stef <stefthedoggo4@gmail.com>
2022-12-27 17:35:18 +0000

    Create selenium-testing.py

---
+++ selenium-testing.py
@@ -0,0 +1,9 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.google.com")
+
+url = 'https://teachablemachine.withgoogle.com/train/image'
